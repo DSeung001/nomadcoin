5cc955d7d41e132b51f6768ad39cc1578d9ffd57
지승렬 <hsnc23032108@hanwha.com>
2023-06-23 17:19:06 +0900

    Pointers

--- main.go
+++ main.go
@@ -2,27 +2,22 @@ package main
 
 import "fmt"
 
-/*
-	Go는 배열이 무한할 수 없음 => C와 유사
-	그래서 Slice 가 존재하며 무한히 커질 수 잇음 => 리스트와 유사
-*/
-
 func main() {
-	foods := [3]string{"potato", "pizza", "pasta"}
-	for _, food := range foods {
-		fmt.Println(food)
-	}
 
-	for i := 0; i < len(foods); i++ {
-		fmt.Println(foods[i])
-	}
+	// 2 출력
+	a := 2
+	b := a
+	a = 12
+	fmt.Println(b)
+	fmt.Println(&a, &b)
 
-	// 크기를 정하지 않으면 slice
-	newFoods := []string{"potato", "pizza", "pasta"}
+	// 둘이 동일
+	c := &b
+	fmt.Println(&b, &c)
 
-	// append는 새로 생성, 직접적으로 바꾸지 않음
-	newFoods2 := append(newFoods, "tomato")
+	// 아래와 같이 기존 변수에는 못하고 새로 정의시 사용 가능
+	//a = &b
 
-	fmt.Printf("%v\n", newFoods)
-	fmt.Printf("%v\n", newFoods2)
+	*c = 123
+	fmt.Println(c, *c, &b, b)
 }
