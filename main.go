6a5de7fde5070ab243172231531355b798d75b9b
지승렬 <hsnc23032108@hanwha.com>
2023-06-23 17:31:34 +0900

    Revert "Structs with Pointers"
    This reverts commit d7f41da272db720da2822ed22c3b033069551dca.

--- main.go
+++ main.go
@@ -2,29 +2,22 @@ package main
 
 import "fmt"
 
-/*
-	Go는 Class나 Object가 없고 대신 Struct가 있음
-*/
-
-type person struct {
-	name string
-	age  int
-}
-
-// receiver 함수
-// Struct을 입력안하면 모든 변수에 유효하지만 여기서는 person만을 지정함
-func (p person) sayHello() {
-	fmt.Printf("Hello! My Name is %s and I'm, %d\n", p.name, p.age)
-}
-
-func (p person) sayHi() {
-	fmt.Printf("안녕 내 이름은 %s이고 내 나이는 %d\n", p.name, p.age)
-}
-
 func main() {
-	// 동일
-	// nico := person{name : "nico", age : 12}
-	nico := person{"nico", 12}
-	nico.sayHello()
-	nico.sayHi()
+
+	// 2 출력
+	a := 2
+	b := a
+	a = 12
+	fmt.Println(b)
+	fmt.Println(&a, &b)
+
+	// 둘이 동일
+	c := &b
+	fmt.Println(&b, &c)
+
+	// 아래와 같이 기존 변수에는 못하고 새로 정의시 사용 가능
+	//a = &b
+
+	*c = 123
+	fmt.Println(c, *c, &b, b)
 }
