cba1cd00220999b869e8bc878c85d3d8c2efcf51
지승렬 <hsnc23032108@hanwha.com>
2023-06-23 16:40:14 +0900

    slices and arrays

--- main.go
+++ main.go
@@ -2,18 +2,27 @@ package main
 
 import "fmt"
 
+/*
+	Go는 배열이 무한할 수 없음 => C와 유사
+	그래서 Slice 가 존재하며 무한히 커질 수 잇음 => 리스트와 유사
+*/
+
 func main() {
-	x := 405940594059
+	foods := [3]string{"potato", "pizza", "pasta"}
+	for _, food := range foods {
+		fmt.Println(food)
+	}
 
-	fmt.Printf("%b\n", x)
-	fmt.Printf("%o\n", x)
-	fmt.Printf("%x\n", x)
-	fmt.Printf("%U\n", x)
+	for i := 0; i < len(foods); i++ {
+		fmt.Println(foods[i])
+	}
 
-	// Format 된 데이터를 반환 함
-	xAsBinary := fmt.Sprintf("%b\n", x)
-	fmt.Println(xAsBinary)
+	// 크기를 정하지 않으면 slice
+	newFoods := []string{"potato", "pizza", "pasta"}
 
-	// 아래 공식 문서에서 확인이 가능
-	// https://pkg.go.dev/fmt
+	// append는 새로 생성, 직접적으로 바꾸지 않음
+	newFoods2 := append(newFoods, "tomato")
+
+	fmt.Printf("%v\n", newFoods)
+	fmt.Printf("%v\n", newFoods2)
 }
