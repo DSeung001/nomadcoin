7155bea90c0388feb813237d77e48e1785a68465
seungryeol <seungryeol156@gmail.com>
2023-06-27 22:24:58 +0900

    Refactoring part Two

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -7,9 +7,9 @@ import (
 )
 
 type block struct {
-	data     string
-	hash     string
-	prevHash string
+	Data     string
+	Hash     string
+	PrevHash string
 }
 
 type blockchain struct {
@@ -23,8 +23,8 @@ var once sync.Once
 
 // calculateHash : 해시 값 계산, 포인터 변수로 직접 변경
 func (b *block) calculateHash() {
-	hash := sha256.Sum256([]byte(b.data + b.prevHash))
-	b.hash = fmt.Sprintf("%x", hash)
+	hash := sha256.Sum256([]byte(b.Data + b.PrevHash))
+	b.Hash = fmt.Sprintf("%x", hash)
 }
 
 // getLastHash : 마지막 해시 값 가져오기
@@ -33,14 +33,19 @@ func getLastHash() string {
 	if totalBlocks == 0 {
 		return ""
 	}
-	return GetBlockchain().blocks[totalBlocks-1].hash
+	return GetBlockchain().blocks[totalBlocks-1].Hash
 }
 
 // createBlock : 블록 생성
 func createBlock(data string) *block {
-	newBlock := block{data: data, hash: "", prevHash: getLastHash()}
+	newBlock := block{Data: data, Hash: "", PrevHash: getLastHash()}
 	newBlock.calculateHash()
 	return &newBlock
+}
+
+// AddBlock : 블록 추가
+func (b *blockchain) AddBlock(data string) {
+	b.blocks = append(b.blocks, createBlock(data))
 }
 
 // GetBlockchain : 블록체인 가져오기
@@ -48,8 +53,12 @@ func GetBlockchain() *blockchain {
 	if b == nil {
 		once.Do(func() {
 			b = &blockchain{}
-			b.blocks = append(b.blocks, createBlock("Genesis Block"))
+			b.AddBlock("Genesis")
 		})
 	}
 	return b
+}
+
+func (b *blockchain) AllBlocks() []*block {
+	return b.blocks
 }
