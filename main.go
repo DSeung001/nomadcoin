dbc9f8d773e4d97ecf78a17535587ef7996dab90
지승렬 <hsnc23032108@hanwha.com>
2023-06-30 10:20:36 +0900

    Parsing Commands

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"github.com/nomadcoders/nomadcoin/rest"
+	"fmt"
+	"os"
 )
 
+func usage() {
+	fmt.Printf("Welcome to 노마드 코인\n\n")
+	fmt.Printf("Please use the following commands:\n\n")
+	fmt.Printf("explorer	: Start the HTML Explorer\n")
+	fmt.Printf("rest		: Start the REST API (recommand)\n\n")
+	os.Exit(0)
+}
+
 func main() {
-	rest.Start(4000)
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	switch os.Args[1] {
+	case "explorer":
+		fmt.Println("Start Explorer")
+	case "rest":
+		fmt.Println("Start Rest API")
+	default:
+		usage()
+	}
 }
