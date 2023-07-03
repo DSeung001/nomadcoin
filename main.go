bd91eed21111f13e6a7f9390b647ec2d7d7550c4
지승렬 <hsnc23032108@hanwha.com>
2023-07-03 13:25:43 +0900

    PoW Proof Of ConCept

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
-	"github.com/nomadcoders/nomadcoin/blockchain"
-	"github.com/nomadcoders/nomadcoin/cli"
-	"github.com/nomadcoders/nomadcoin/db"
+	"crypto/sha256"
+	"fmt"
+	"strings"
 )
 
 func main() {
-	// defer 은 함수가 종료시 실행
-	defer db.Close()
+	// 난이도
+	difficulty := 3
 
-	blockchain.Blockchain()
-	cli.Start()
+	// 앞에 있어야하는 문자열
+	target := strings.Repeat("0", difficulty)
+
+	// 해시키
+	nonce := 1
+
+	for {
+		hash := fmt.Sprintf("%x", sha256.Sum256([]byte("hello"+fmt.Sprint(nonce))))
+		fmt.Printf("Hash:%s\nTarget:%s\nNonce:%d\n\n", hash, target, nonce)
+		if strings.HasPrefix(hash, target) {
+			return
+		} else {
+			nonce++
+		}
+
+	}
 }
