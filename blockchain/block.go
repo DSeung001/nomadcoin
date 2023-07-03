dfe1f57586a29aed310ecebab811e7ced07816e2
지승렬 <hsnc23032108@hanwha.com>
2023-07-03 14:56:42 +0900

    Difficulty part One

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,15 +1,12 @@
 package blockchain
 
 import (
-	"crypto/sha256"
 	"errors"
-	"fmt"
 	"github.com/nomadcoders/nomadcoin/db"
 	"github.com/nomadcoders/nomadcoin/utils"
 	"strings"
+	"time"
 )
-
-const difficulty int = 2
 
 type Block struct {
 	Data       string `json:"data"`
@@ -18,6 +15,7 @@ type Block struct {
 	Height     int    `json:"height"`
 	Difficulty int    `json:"difficulty"`
 	Nonce      int    `json:"nonce"`
+	Timestamp  int    `json:"timestamp"`
 }
 
 func (b *Block) persist() {
@@ -44,11 +42,11 @@ func FindBlock(hash string) (*Block, error) {
 func (b *Block) mine() {
 	target := strings.Repeat("0", b.Difficulty)
 	for {
-		blockAsString := fmt.Sprint(b)
-		hash := fmt.Sprintf("%x", sha256.Sum256([]byte(blockAsString)))
-		//fmt.Printf("Blcok as String:%s\n Hash:%s\nTarget:%s\nNonce:%d\n\n\n", blockAsString, hash, target, b.Nonce)
+		hash := utils.Hash(b)
+		//fmt.Printf("Block as String:%s\n Hash:%s\nTarget:%s\nNonce:%d\n\n\n", blockAsString, hash, target, b.Nonce)
 		if strings.HasPrefix(hash, target) {
 			b.Hash = hash
+			b.Timestamp = int(time.Now().Unix())
 			break
 		} else {
 			b.Nonce++
@@ -62,12 +60,9 @@ func createBlock(data string, prevHash string, height int) *Block {
 		Hash:       "",
 		PrevHash:   prevHash,
 		Height:     height,
-		Difficulty: difficulty,
+		Difficulty: Blockchain().difficulty(),
 		Nonce:      0,
 	}
-	payload := block.Data + block.PrevHash + string(rune(block.Height))
-	block.Hash = fmt.Sprintf("%x", sha256.Sum256([]byte(payload)))
-
 	block.mine()
 	block.persist()
 	return &block
