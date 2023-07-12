69c070c4a1ef8e40eff6c945f7fcf9be7ee15302
지승렬 <hsnc23032108@hanwha.com>
2023-07-12 16:54:32 +0900

    Private and Public Keys

--- wallet/wallert.go
+++ wallet/wallert.go
@@ -1,1 +1,14 @@
 package wallet
+
+1) we hash the msg.
+	"i love you" -> hash(x) -> "hashed_message"
+
+2) generate key pair
+	KeyPair (privateKey, publicKey)
+	(save privateKey to afile(wallet))
+
+3) sign the hash
+	("hashed_message" + privateKey) -> "signature"
+
+4) verfiy
+	("hashed_message" + "signature" + publicKey) -> true|false
