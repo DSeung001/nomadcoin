1adf629f6310caf1c7a121d0c29066882748537b
지승렬 <hsnc23032108@hanwha.com>
2023-07-24 16:12:35 +0900

    openPort

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gorilla/websocket"
 	"github.com/nomadcoders/nomadcoin/utils"
 	"net/http"
+	"strings"
 )
 
 var upgrader = websocket.Upgrader{}
@@ -18,13 +19,17 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 	}
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
-	initPeer(conn, "xx", "xx")
+	openPort := r.URL.Query().Get("openPort")
+	fmt.Println(r.RemoteAddr)
+
+	result := strings.Split(r.RemoteAddr, ":")
+	initPeer(conn, result[0], openPort)
 }
 
-func AddPeer(address, port string) {
+func AddPeer(address, port, openPort string) {
 	// Port :4000 is requesting an upgrade from the port :3000
 	// Go에서 websocket 요청보내기
-	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws", address, port), nil)
+	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort), nil)
 	utils.HandleErr(err)
 	initPeer(conn, address, port)
 }
