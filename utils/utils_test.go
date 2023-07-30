aa96f14525a59fbdd66e61feee87cd6c1ee67c85
seungryeol <seungryeol156@gmail.com>
2023-07-30 14:38:26 +0900

    Coverage

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -3,10 +3,13 @@ package utils
 import (
 	"encoding/hex"
 	"fmt"
+	"reflect"
 	"testing"
 )
 
 // go test ./... -v <= 전체 파일 테스팅
+// go test -v -coverprofile cover.out ./... <= 테스팅 결고 파일로 생성
+// go tool cover -html="cover.out" 로 파일이 아닌 html 확인이 가능
 
 func TestHash(t *testing.T) {
 	hash := "e005c1d727f7776a57a661d61a182816d8953c0432780beeae35e337830b1746"
@@ -35,4 +38,13 @@ func ExampleHash() {
 	x := Hash(s)
 	fmt.Println(x)
 	// Output: e005c1d727f7776a57a661d61a182816d8953c0432780beeae35e337830b1746
+}
+
+func TestToBytes(t *testing.T) {
+	s := "test"
+	b := ToBytes(s)
+	k := reflect.TypeOf(b).Kind()
+	if k != reflect.Slice {
+		t.Errorf("ToBytes should return a slice of bytes %s", k)
+	}
 }
